888589605c3e2dabbbfe82a0412b77291e035ad7
gbargougui <ghassen.brg16@gmail.com>
2023-05-11 01:48:59 +0100

    dockerize the product-catalog-service

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 mvn clean install -DskipTests & ^
 docker build -t user-management-service:1.0.0-SNAPSHOT -f ./user-management-service/Dockerfile ./user-management-service & ^
+docker build -t product-catalog-service:1.0.0-SNAPSHOT -f ./product-catalog-service/Dockerfile ./product-catalog-service & ^
 docker build -t dummy-service:1.0.0-SNAPSHOT -f ./dummy-service/Dockerfile ./dummy-service & ^
 docker build -t eureka-server:1.0.0-SNAPSHOT -f ./eureka-server/Dockerfile ./eureka-server & ^
 docker build -t gateway:1.0.0-SNAPSHOT -f ./gateway/Dockerfile ./gateway
--- product-catalog-service/src/app.module.ts
+++ product-catalog-service/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/product-catalog-service'),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
