1292b766defa2780953624253ddedb23997fc9c5
marwenlahmer <marwen.lahmar97@gmail.com>
2023-05-11 23:12:36 +0100

    feat(order-processing): configure order processing service

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 mvn clean install -DskipTests & ^
 docker build -t user-management-service:1.0.0-SNAPSHOT -f ./user-management-service/Dockerfile ./user-management-service & ^
 docker build -t product-catalog-service:1.0.0-SNAPSHOT -f ./product-catalog-service/Dockerfile ./product-catalog-service & ^
+docker build -t order-processing-service:1.0.0-SNAPSHOT -f ./order-processing-service/Dockerfile ./order-processing-service & ^
 docker build -t dummy-service:1.0.0-SNAPSHOT -f ./dummy-service/Dockerfile ./dummy-service & ^
 docker build -t eureka-server:1.0.0-SNAPSHOT -f ./eureka-server/Dockerfile ./eureka-server & ^
 docker build -t gateway:1.0.0-SNAPSHOT -f ./gateway/Dockerfile ./gateway
