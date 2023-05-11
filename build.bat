66f458815cd35270c3c3b04d4ebec8ce0219b6a3
marwenlahmer <marwen.lahmar97@gmail.com>
2023-05-11 23:34:00 +0100

    feat(order-processing) configure order processing service

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 mvn clean install -DskipTests & ^
 docker build -t user-management-service:1.0.0-SNAPSHOT -f ./user-management-service/Dockerfile ./user-management-service & ^
 docker build -t product-catalog-service:1.0.0-SNAPSHOT -f ./product-catalog-service/Dockerfile ./product-catalog-service & ^
+docker build -t order-processing-service:1.0.0-SNAPSHOT -f ./order-processing-service/Dockerfile ./order-processing-service & ^
 docker build -t config-server:1.0.0-SNAPSHOT -f ./config-server/Dockerfile ./config-server & ^
 docker build -t payment-service:1.0.0-SNAPSHOT -f ./payment-service/Dockerfile ./payment-service & ^
 docker build -t dummy-service:1.0.0-SNAPSHOT -f ./dummy-service/Dockerfile ./dummy-service & ^
