daa3636f66b3583e758cf5ee9391f0e048b2e520
Mohamed Dhia Hachem <dhiua99@gmail.com>
2023-05-13 11:15:49 +0200

    fix stmp bug

--- payment-service/src/main/java/tn/esprit/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/tn/esprit/payment/PaymentServiceApplication.java
@@ -3,13 +3,28 @@ package tn.esprit.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
+import feign.RequestInterceptor;
+
+@Configuration
 @EnableFeignClients
 @SpringBootApplication
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+	@Bean
+	public RequestInterceptor requestInterceptor() {
+		return requestTemplate -> {
+			requestTemplate.header("Authorization", "Bearer "
+					+ ((Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getTokenValue());
+		};
 	}
 
 }
